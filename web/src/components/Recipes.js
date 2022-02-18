b2746cebec88133148e46de0f86ed0b485149fa1
Shawn F Lewis <44281309+sflewis2970@users.noreply.github.com>
2022-02-17 17:02:30 -0800

    Update Recipes.js
    Updated the name of the property being passed in.

--- web/src/components/Recipes.js
+++ web/src/components/Recipes.js
@@ -3,13 +3,12 @@
 // Import custom components
 import Recipe from './Recipe'
 
-const Recipes = ({ recipes, toggleRecipes, onDelete }) => {
-
+const Recipes = ({ recipes, onToggle, onDelete }) => {
   return (
     <>
       <div className="recipes">
         {recipes.map((recipe, idx) => (
-          <Recipe recipe={recipe} key={recipe.id} index={recipe.id} toggleRecipes={toggleRecipes} onDelete={onDelete} />
+          <Recipe key={idx} recipe={recipe} onToggle={onToggle} onDelete={onDelete} />
         ))}
       </div>
     </>
